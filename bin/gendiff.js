0a2c6c6592685789a41ea7b1d8a6bc0b92ecf2da
sulianova <ulianova.sofia@gmail.com>
2022-11-25 19:36:26 +0600

    Update 1 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,10 +8,6 @@ program
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });
+  .option('-V, --version', 'output the version number');
 
 program.parse();
