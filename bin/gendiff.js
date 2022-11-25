324f1048702bca300a78c601fe9d5328c0474bc5
sulianova <ulianova.sofia@gmail.com>
2022-11-25 19:49:20 +0600

    Fix step 1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,10 +4,10 @@ import { program } from 'commander';
 // import version from '../package.json';
 
 program
-  .name('gendiff');
+  .name('gendiff')
+  .version('1.0.0');
 
 program
-  .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .description('Compares two configuration files and shows a difference.');
 
 program.parse();
