c56ea961ecd3ebb90cc2472fa937f511c23cb0f9
sulianova <ulianova.sofia@gmail.com>
2022-11-26 19:00:39 +0600

    Fix file structure

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -5,13 +5,13 @@ import genDiff from '../src/index.js';
 const fileExt = ['.json'];
 
 const resultStylish = fs.readFileSync(
-  path.resolve(process.cwd(), '__tests__/__fixtures__/result_stylish.txt'),
+  path.resolve(process.cwd(), '__fixtures__/result_stylish.txt'),
   'utf-8',
 );
 
 test.each(fileExt)('testing different file options', (extension) => {
-  const fileBefore = `__tests__/__fixtures__/file1${extension}`;
-  const fileAfter = `__tests__/__fixtures__/file2${extension}`;
+  const fileBefore = `__fixtures__/file1${extension}`;
+  const fileAfter = `__fixtures__/file2${extension}`;
   const actual1 = genDiff(fileBefore, fileAfter, 'stylish');
   expect(actual1).toEqual(resultStylish);
   const actual2 = genDiff(fileBefore, fileAfter);
