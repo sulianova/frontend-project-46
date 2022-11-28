066d9d42553b081efa1ca100e1a138a596bacd98
sulianova <ulianova.sofia@gmail.com>
2022-11-28 21:06:58 +0600

    Fix jest test for json format

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -14,6 +14,6 @@ test.each(fileExt)('Testing different file options', (extension) => {
   const fileAfter = `${__dirname}/file2${extension}`;
   expect(genDiff(fileBefore, fileAfter, 'stylish')).toEqual(readFile('result_stylish.txt'));
   expect(genDiff(fileBefore, fileAfter, 'plain')).toEqual(readFile('result_plain.txt'));
-  expect(genDiff(fileBefore, fileAfter, 'json')).toEqual(readFile('result_json.txt'));
+  expect(() => JSON.parse(genDiff(fileBefore, fileAfter, 'json'))).not.toThrow();
   expect(genDiff(fileBefore, fileAfter)).toEqual(readFile('result_stylish.txt'));
 });
