e2ae718f64c2449cfbdf78e6e62e3af513c9e44a
sulianova <ulianova.sofia@gmail.com>
2022-11-28 18:22:03 +0600

    Fix compareData in line with codeclimate

--- src/compareData.js
+++ src/compareData.js
@@ -1,61 +1,38 @@
 import _ from 'lodash';
 
-const sorteKeys = (obj1, obj2) => {
+const sortKeys = (obj1, obj2) => {
   const keys1 = _.keys(obj1);
   const keys2 = _.keys(obj2);
   return _.sortBy(_.union(keys1, keys2));
 };
 
-const setKeyType = (key, obj1, obj2) => {
-  if (!_.has(obj1, key)) return 'added';
-  if (!_.has(obj2, key)) return 'deleted';
-  if (obj1[key] !== obj2[key]) return 'changed';
-  if (_.isObject(obj1[key]) && _.isObject(obj2[key])) return 'nested';
-  return 'unchanged';
-};
+const makeObj = (key, type, value, children, valueBefore, valueAfter) => ({
+  key,
+  type,
+  value,
+  children,
+  valueBefore,
+  valueAfter,
+});
 
 const compareData = (obj1, obj2) => {
-  const sortedKeys = sorteKeys(obj1, obj2);
+  const sortedKeys = sortKeys(obj1, obj2);
 
   const result = sortedKeys.map((key) => {
-    const keyType = setKeyType(key, obj1, obj2);
-    switch (keyType) {
-      case 'added':
-        return {
-          key,
-          value: obj2[key],
-          type: 'added',
-        };
-      case 'deleted':
-        return {
-          key,
-          value: obj1[key],
-          type: 'deleted',
-        };
-      case 'nested':
-        return {
-          key,
-          type: 'nested',
-          children: compareData(obj1[key], obj2[key]),
-        };
-      case 'changed':
-        return {
-          key,
-          valueBefore: obj1[key],
-          valueAfter: obj2[key],
-          type: 'changed',
-        };
-      case 'unchanged':
-        return {
-          key,
-          value: obj1[key],
-          type: 'unchanged',
-        };
+    const obj = makeObj(key);
+    switch (true) {
+      case (!_.has(obj1, key)):
+        return { ...obj, type: 'added', value: obj2[key] };
+      case (!_.has(obj2, key)):
+        return { ...obj, type: 'deleted', value: obj1[key] };
+      case (_.isObject(obj1[key]) && _.isObject(obj2[key])):
+        return { ...makeObj(key, 'nested'), children: compareData(obj1[key], obj2[key]) };
+      case (obj1[key] !== obj2[key]):
+        return { ...makeObj(key, 'changed'), valueBefore: obj1[key], valueAfter: obj2[key] };
       default:
-        throw new Error(`${keyType} is not supported`);
+        return { ...obj, type: 'unchanged', value: obj1[key] };
     }
   });
-
   return result;
 };
 
