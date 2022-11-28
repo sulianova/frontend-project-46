b94a6260f1ec8927edc8595e703a45414c4df853
sulianova <ulianova.sofia@gmail.com>
2022-11-28 20:58:46 +0600

    Fix jest tests

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -9,30 +9,11 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test.each(fileExt)('Stylish: testing different file options.', (extension) => {
+test.each(fileExt)('Testing different file options', (extension) => {
   const fileBefore = `${__dirname}/file1${extension}`;
   const fileAfter = `${__dirname}/file2${extension}`;
-  const actual1 = genDiff(fileBefore, fileAfter, 'stylish');
-  expect(actual1).toEqual(readFile('result_stylish.txt'));
-});
-
-test.each(fileExt)('Plain: testing different file options', (extension) => {
-  const fileBefore = `${__dirname}/file1${extension}`;
-  const fileAfter = `${__dirname}/file2${extension}`;
-  const actual2 = genDiff(fileBefore, fileAfter, 'plain');
-  expect(actual2).toEqual(readFile('result_plain.txt'));
-});
-
-test.each(fileExt)('Json: testing different file options', (extension) => {
-  const fileBefore = `${__dirname}/file1${extension}`;
-  const fileAfter = `${__dirname}/file2${extension}`;
-  const actual2 = genDiff(fileBefore, fileAfter, 'json');
-  expect(actual2).toEqual(readFile('result_json.txt'));
-});
-
-test.each(fileExt)('Default: testing different file options', (extension) => {
-  const fileBefore = `${__dirname}/file1${extension}`;
-  const fileAfter = `${__dirname}/file2${extension}`;
-  const actual3 = genDiff(fileBefore, fileAfter);
-  expect(actual3).toEqual(readFile('result_stylish.txt'));
+  expect(genDiff(fileBefore, fileAfter, 'stylish')).toEqual(readFile('result_stylish.txt'));
+  expect(genDiff(fileBefore, fileAfter, 'plain')).toEqual(readFile('result_plain.txt'));
+  expect(genDiff(fileBefore, fileAfter, 'json')).toEqual(readFile('result_json.txt'));
+  expect(genDiff(fileBefore, fileAfter)).toEqual(readFile('result_stylish.txt'));
 });
