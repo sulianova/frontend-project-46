b50f819de03f4887f781d6a5da19eb19d43aca88
sulianova <ulianova.sofia@gmail.com>
2022-11-28 20:51:52 +0600

    Add json render

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -7,27 +7,32 @@ const __filename = '__fixtures__/result_stylish.txt';
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-const resultStylish = fs.readFileSync(getFixturePath('result_stylish.txt'), 'utf-8');
-const resultPlain = fs.readFileSync(getFixturePath('result_plain.txt'), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test.each(fileExt)('Stylish: testing different file options.', (extension) => {
   const fileBefore = `${__dirname}/file1${extension}`;
   const fileAfter = `${__dirname}/file2${extension}`;
   const actual1 = genDiff(fileBefore, fileAfter, 'stylish');
-  expect(actual1).toEqual(resultStylish);
+  expect(actual1).toEqual(readFile('result_stylish.txt'));
 });
 
 test.each(fileExt)('Plain: testing different file options', (extension) => {
   const fileBefore = `${__dirname}/file1${extension}`;
   const fileAfter = `${__dirname}/file2${extension}`;
   const actual2 = genDiff(fileBefore, fileAfter, 'plain');
-  expect(actual2).toEqual(resultPlain);
+  expect(actual2).toEqual(readFile('result_plain.txt'));
+});
+
+test.each(fileExt)('Json: testing different file options', (extension) => {
+  const fileBefore = `${__dirname}/file1${extension}`;
+  const fileAfter = `${__dirname}/file2${extension}`;
+  const actual2 = genDiff(fileBefore, fileAfter, 'json');
+  expect(actual2).toEqual(readFile('result_json.txt'));
 });
 
 test.each(fileExt)('Default: testing different file options', (extension) => {
   const fileBefore = `${__dirname}/file1${extension}`;
   const fileAfter = `${__dirname}/file2${extension}`;
   const actual3 = genDiff(fileBefore, fileAfter);
-  expect(actual3).toEqual(resultStylish);
+  expect(actual3).toEqual(readFile('result_stylish.txt'));
 });
--- src/renders/index.js
+++ src/renders/index.js
@@ -1,6 +1,6 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
-// import json from './json.js';
+import json from './json.js';
 
 const format = (data, formatName) => {
   switch (formatName) {
@@ -10,9 +10,9 @@ const format = (data, formatName) => {
     case 'plain': {
       return plain(data);
     }
-    // case 'json': {
-    //   return json(data);
-    // }
+    case 'json': {
+      return json(data);
+    }
     default:
       throw new Error('Invalid format');
   }
